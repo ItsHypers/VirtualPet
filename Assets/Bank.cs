ee0639ea1c2cb37311ab45375734e636dba4fa37
ItsHypers <ryanathomas013@gmail.com>
2022-01-07 18:29:34 +0000

    added money

--- Assets/Bank.cs
+++ Assets/Bank.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class Bank : MonoBehaviour
 {
     public float Money;
+    public TMP_Text text;
+    private string currency;
 
     private void Start()
     {
-        Money = 0.0f;
+        Money = 0.1f;
+        if(!PlayerPrefs.HasKey("currency"))
+        {
+            PlayerPrefs.SetString("currency", "£");
+        }
+    }
+    private void Update()
+    {
+        text.text = PlayerPrefs.GetString("currency") + Money.ToString();
     }
 }
